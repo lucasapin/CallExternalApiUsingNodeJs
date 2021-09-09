42a5b9a648d23c07f6fc2d7def822be4c5a2bff3
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-09-09 15:57:47 -0400

    Call shopify api

--- NodeJsCall.js
+++ NodeJsCall.js
@@ -1,28 +1,27 @@
+const https = require("https");
 
-const https = require('https');
- 
-_EXTERNAL_URL = 'https://test-proj-heroku.herokuapp.com/api/plans';
-
+_EXTERNAL_URL =
+  "https://8822ca43d5557897e478a23b5b8b54eb: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@maison-de-vic.myshopify.com/admin/api/2021-07/orders.json?status=any";
 
 const callExternalApiUsingHttp = (callback) => {
-    https.get(_EXTERNAL_URL, (resp) => {
-    let data = '';
-    
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
+  https
+    .get(_EXTERNAL_URL, (resp) => {
+      let data = "";
+
+      // A chunk of data has been recieved.
+      resp.on("data", (chunk) => {
         data += chunk;
-    });
-    
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on("end", () => {
         return callback(data);
-       // console.log(JSON.stringify(data));
+        // console.log(JSON.stringify(data));
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
     });
-    
-    }).on("error", (err) => {
-       
-    console.log("Error: " + err.message);
-    });
-}
+};
 
 module.exports.callApi = callExternalApiUsingHttp;
--- Request.js
+++ Request.js
@@ -1,14 +1,15 @@
-const request = require('request');
- 
-_EXTERNAL_URL = 'https://test-proj-heroku.herokuapp.com/api/plans';
+const request = require("request");
+
+_EXTERNAL_URL =
+  "https://8822ca43d5557897e478a23b5b8b54eb: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@maison-de-vic.myshopify.com/admin/api/2021-07/orders.json?status=any";
 
 const callExternalApiUsingRequest = (callback) => {
-    request(_EXTERNAL_URL, { json: true }, (err, res, body) => {
-    if (err) { 
-        return callback(err);
-     }
+  request(_EXTERNAL_URL, { json: true }, (err, res, body) => {
+    if (err) {
+      return callback(err);
+    }
     return callback(body);
-    });
-}
+  });
+};
 
 module.exports.callApi = callExternalApiUsingRequest;
